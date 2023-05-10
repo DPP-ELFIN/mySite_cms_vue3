b58d77360a3041c06dea49ab0fdde67d74f7755b
zwp <1519037604@qq.com.com>
2023-05-10 17:35:08 +0800

    404页面

--- src/view/404/NotFound.vue
+++ src/view/404/NotFound.vue
@@ -1,12 +1,29 @@
 <template>
-    <div class="not_found">123</div>
+    <div class="not_found">
+        <img src="@/assets/img/404.png" alt="">
+        <h1>页面未找到！</h1>
+        <el-button type="danger" size="large" @click="goBack">返回</el-button>
+    </div>
 </template>
 
 
 <script setup lang='ts'>
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+const goBack = () => {
+    router.back()
+}
 
 </script>
 
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.not_found {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 30px;
+}
+</style>
